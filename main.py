24994024da217cc58ccbe4d6284752b9941e5496
rukh-debug <talkto@rubenk.com.np>
2024-06-02 07:18:50 -0400

    fix: minimal conditional error handeling

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import requests
 import json
 import pandas as pd
 
+auth = str(input("Enter Authorization: "))
 
+channelid = int(input("Enter The Channel ID you want to export chats from: ")) #Channel ID
 
-auth = str(input("Enter Authorization\n"))
-
-channelid = int(input("Enter The Channel ID you want to export chats from\n")) #Channel ID
-
-limit = int(input("Enter the number of last messages you wanna export \n"))  #Number of Last Messages
+limit = int(input("Enter the number of last messages you wanna export: "))  #Number of Last Messages
 
 data = [] 
 
@@ -17,20 +15,26 @@ def retrieve_messages(channelid):
         'authorization': auth
     }
     r= requests.get(f'https://discord.com/api/v9/channels/{channelid}/messages?limit={limit}', headers=headers)
-    json_loader = json.loads(r.text)
-    for value in json_loader:
-        liyeko = value['content']
-        # print(value['content'], '\n')
-        i = 1
-        while i < limit:
-            i +=1
-            data.insert(i, liyeko)
-            break
-    
-    print(data)
-    df = pd.DataFrame(data)
-    df.to_excel("retrieved_messages.xlsx")
+    # Error handeling
+    if r.status_code == 200:
+        print(r)
+        json_loader = json.loads(r.text)
+        for value in json_loader:
+            print(value)
+            liyeko = value['content']
+            # print(value['content'], '\n')
+            i = 1
+            while i < limit:
+                i +=1
+                data.insert(i, liyeko)
+                break
+        
+        print(data)
+        df = pd.DataFrame(data)
+        df.to_excel("retrieved_messages.xlsx")
+    else:
+        print(f"Error: {r.status_code}")
+        exit()
     
    
-
 retrieve_messages(channelid)
